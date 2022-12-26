a645c4957118a0e56cb6fcd4c6d8d34deb34af7e
hanashah-01 <88876401+hanashah-01@users.noreply.github.com>
2022-12-26 18:17:09 +0800

    First Commit

---
+++ lib/screens/login.dart
@@ -0,0 +1,149 @@
+import 'package:flutter/material.dart';
+import 'package:teamone_app/utils/colors_util.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> with SingleTickerProviderStateMixin {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: hexStringToColor("E5E5E5"),
+      body:SafeArea(
+        child: Column(
+            children: [
+              SizedBox(height: 36),
+
+              Padding(
+                padding: const EdgeInsets.only(right:320),
+                child: Image.asset("assets/images/back_button.png", height: 41, width: 41),
+              ),
+
+              SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.only(right: 25),
+            child: Text(
+        'Welcome Back! Glad\nto see you again!',
+            style: TextStyle(
+              fontFamily: 'Urbanist',
+              fontWeight: FontWeight.bold,
+            fontSize: 35,
+        )),
+          ),
+SizedBox(height: 20),
+
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    border: Border.all(color: Colors.white),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border:InputBorder.none,
+                        hintText: 'Enter your student e-mail',
+                      )
+                    ),
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 10),
+
+              Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey[200],
+                      border: Border.all(color: Colors.white),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 20),
+                      child: TextField(
+                        obscureText: true,
+                          decoration: InputDecoration(
+                            border:InputBorder.none,
+                            hintText: 'Enter your password',
+                          )
+                      ),
+                    ),
+                  )
+              ),
+              SizedBox(height: 15),
+
+              Padding(
+                padding: const EdgeInsets.only(right: 22),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      "Forgot Password?",
+                      style: TextStyle(
+                        fontFamily: 'Urbanist',
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              SizedBox(height: 20),
+
+              Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: Container(
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.black87,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Center(
+                  child: Text(
+                    'Login',
+                    style: TextStyle(
+                      fontFamily: 'Urbanist',
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+              ),),
+
+              SizedBox(height: 180),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Don't have an account?",
+                    style: TextStyle(
+                      fontFamily: 'Urbanist',
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    ' Register Now',
+                    style: TextStyle(
+                      fontFamily: 'Urbanist',
+                      color: hexStringToColor("9E88B2"),
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+        ]),
+      ),
+    );
+
+  }
+}
